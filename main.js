9b47debbd7764a80fde08b0711742fa8757e7d46
Saeed Norouzi <saeednorouzi98@gmail.com>
2024-03-22 12:14:41 +0330

    Feat(swagger): add swagger config
    Description:
    In order to make a document for the application's APIs, I
    have added the config of the swagger API doc system to the
    project.
    
    Change Logs:
    [Packages][Added] - Add swagger ui and jsdoc packages.
    [ENVs][Modified] - Add Application Domain address.
    [Swagger config][Added] - Add swagger config file.

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 /** import express */
-const express = require('express');
+const express = require("express");
 /** import dotenv */
 const dotenv = require("dotenv");
 /** import path */
-const path = require('path');
+const path = require("path");
+/** import swagger configs */
+const SwaggerConfig = require("./src/config/swagger.config");
 /** config project's env file */
 dotenv.config({
 	path: path.resolve(`./env/.env.${process.env.NODE_ENV}`),
@@ -22,9 +24,12 @@ async function main() {
 	/** import mongodb connection config */
 	require("./src/config/mongoose.config");
 
+	/** initialize swagger */
+	SwaggerConfig(app);
+
 	app.listen(PORT, () => {
 		console.log(`Server: http://localhost:${PORT}`);
 	});
 }
 
-main()
+main();
---
+++ src/config/swagger.config.js
@@ -0,0 +1,39 @@
+/** import swagger jsdoc */
+const swaggerJsDoc = require("swagger-jsdoc");
+/** import swagger ui */
+const swaggerUi = require("swagger-ui-express");
+
+/**
+ * initialize swagger
+ * @param {object} app - application instants created from express
+ */
+const SwaggerConfig = (app) => {
+	/** define swagger doc config */
+	const swaggerDocument = swaggerJsDoc({
+		swaggerDefinition: {
+			/** define swagger's open api version */
+			openapi: "3.0.1",
+			/** define the info shown in swagger page */
+			info: {
+				title: "Divar Application",
+				description: "Divar Application's APIs document",
+				version: "1.0.0",
+			},
+			/** set server url based on application runtime environment */
+			servers: [
+				{
+					url: process.env.APPLICATION_DOMAIN,
+				},
+			],
+		},
+		/** set the rute of swagger docs */
+		apis: [process.cwd() + "src/modules/**/*.swagger.js"],
+	});
+
+	/** initialize swagger UI */
+	const swagger = swaggerUi.setup(swaggerDocument, {});
+	/** initialize swagger ui route */
+	app.use("/api-doc", swaggerUi.serve, swagger);
+};
+
+module.exports = SwaggerConfig;
