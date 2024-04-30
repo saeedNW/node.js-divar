0fcb4584087722c70de0834cfff1a3580cb25e60
Saeed Norouzi <saeednorouzi98@gmail.com>
2024-04-30 16:59:21 +0330

    Feat(view engine): add ejs view engine into application
    Description:
    So that the application can serve and render web pages to users, here in
    this commit, I have included the EJS view engine in the application.
    
    Change Logs:
    [Packages][Added] - Install ejs and express ejs layouts modules.
    [Main configs][Modified] - Initialize the view engine.
    [Views][Addede] - Add main layout files and pages.

--- main.js
+++ main.js
@@ -16,6 +16,8 @@ const { NotFoundHandler } = require("./src/common/exceptions/notfound.handler");
 const { ErrorsHandler } = require("./src/common/exceptions/error.handler");
 /** import cookie parser */
 const cookieParser = require("cookie-parser");
+/** import express-ejs-layouts */
+const expressEjsLayouts = require("express-ejs-layouts");
 
 async function main() {
 	/** create an app instants from express */
@@ -34,8 +36,21 @@ async function main() {
 	app.use(express.json());
 	/** initialize express urlencoded body parser */
 	app.use(express.urlencoded({ extended: true }));
+	/** initialize express statics */
+	app.use(express.static(process.cwd() + "/public"));
 	/** initialize cookieParser module */
 	app.use(cookieParser(process.env.COOKIE_PARSER_SECRET));
+
+	/** initialize express ejs layouts */
+	app.use(expressEjsLayouts);
+	/** initialize View Engine */
+	app.set("view engine", "ejs");
+	/** define application main layout file */
+	app.set("layout", "./layouts/panel/main");
+	/** enable express-ejs-layouts extractScripts feature */
+	app.set("layout extractScripts", true);
+	/** enable express-ejs-layouts extractStyles feature */
+	app.set("layout extractStyles", true);
 
 	/** initialize swagger */
 	SwaggerConfig(app);
